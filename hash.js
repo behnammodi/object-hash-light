241a81890f839dcb1d5c88c1d05a15be83826073
Behnam Mohammadi <itten@live.com>
2021-08-21 10:01:33 +0430

    fix: remove build file

--- hash.js
+++
@@ -1,9 +0,0 @@
-async function hash(data, option = { algorithm: "SHA-1" }) {
-    const uint8 = new TextEncoder().encode(JSON.stringify(data));
-    const buffer = await crypto.subtle.digest(option.algorithm, uint8);
-    const hex = Array.from(new Uint8Array(buffer))
-        .map((b) => b.toString(16).padStart(2, "0"))
-        .join("");
-    return hex;
-}
-export default hash;
