ebe2c8dad90a9e03e109c758cb7ad30c4d0da9e0
Behnam Mohammadi <itten@live.com>
2021-08-21 09:51:49 +0430

    1.0.5

--- hash.js
+++ hash.js
@@ -1,6 +1,6 @@
 async function hash(data, option = { algorithm: "SHA-1" }) {
-    const msgUint8 = new TextEncoder().encode(JSON.stringify(data));
-    const buffer = await crypto.subtle.digest(option.algorithm, msgUint8);
+    const uint8 = new TextEncoder().encode(JSON.stringify(data));
+    const buffer = await crypto.subtle.digest(option.algorithm, uint8);
     const hex = Array.from(new Uint8Array(buffer))
         .map((b) => b.toString(16).padStart(2, "0"))
         .join("");
